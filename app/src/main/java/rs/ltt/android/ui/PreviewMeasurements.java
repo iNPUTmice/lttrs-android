6545025ffa179fbc7db5f50a16e49b04dfc9564c
Daniel Gultsch <daniel@gultsch.de>
2022-01-21 13:23:44 +0100

    add utility to calculate desired preview dimensions

---
+++ app/src/main/java/rs/ltt/android/ui/PreviewMeasurements.java
@@ -0,0 +1,91 @@
+package rs.ltt.android.ui;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+
+public class PreviewMeasurements {
+
+    public final int sampleSize;
+    public final int x;
+    public final int y;
+    public final int width;
+    public final int height;
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("sampleSize", sampleSize)
+                .add("x", x)
+                .add("y", y)
+                .add("width", width)
+                .add("height", height)
+                .toString();
+    }
+
+    private PreviewMeasurements(
+            final int sampleSize, final int x, final int y, final int width, final int height) {
+        this.sampleSize = sampleSize;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public static PreviewMeasurements of(
+            final int inWidth, final int inHeight, final int outWidth, final int outHeight) {
+        Preconditions.checkArgument(
+                outWidth > outHeight,
+                "This method is optimized to produce landscape preview. OutWidth must be greater"
+                        + " than outHeight");
+        final int sampleSize = calculateSampleSize(inWidth, inHeight, outWidth, outHeight);
+        if (sampleSize > 1) {
+            final PreviewMeasurements measurements =
+                    of(inWidth / sampleSize, inHeight / sampleSize, outWidth, outHeight);
+            if (measurements.sampleSize != 1) {
+                throw new IllegalStateException("Sample size should have been 1 after second pass");
+            }
+            return new PreviewMeasurements(
+                    sampleSize,
+                    measurements.x,
+                    measurements.y,
+                    measurements.width,
+                    measurements.height);
+        }
+        if (inWidth < outWidth && inHeight < outHeight) {
+            return new PreviewMeasurements(sampleSize, 0, 0, inWidth, inHeight);
+        }
+        // landscape and square images get their preview cut from the middle
+        final float targetAspectRatio = (float) outWidth / outHeight;
+        final boolean isInLandscape = inWidth >= inHeight;
+        final int optimalHeight = Math.round(inWidth / targetAspectRatio);
+        if (isInLandscape) {
+            final float inAspectRatio = (float) inWidth / inHeight;
+            if (inAspectRatio > targetAspectRatio) {
+                final int widthDifference = inWidth - outWidth;
+                final int height = Math.min(optimalHeight, inHeight);
+                return new PreviewMeasurements(
+                        sampleSize, widthDifference / 2, 0, outWidth, height);
+            } else {
+                final int heightDifference = optimalHeight - outHeight;
+                return new PreviewMeasurements(
+                        sampleSize, 0, heightDifference / 2, inWidth, optimalHeight);
+            }
+        } else {
+            final int height = Math.max(optimalHeight, outHeight);
+            return new PreviewMeasurements(sampleSize, 0, 0, inWidth, height);
+        }
+    }
+
+    private static int calculateSampleSize(
+            final int inWidth, final int inHeight, final int outWidth, final int outHeight) {
+        int sampleSize = 1;
+        if (inHeight > outHeight || inWidth > outWidth) {
+            final int halfHeight = inHeight / 2;
+            final int halfWidth = inWidth / 2;
+            while ((halfHeight / sampleSize) >= outHeight && (halfWidth / sampleSize) >= outWidth) {
+                sampleSize *= 2;
+            }
+        }
+        return sampleSize;
+    }
+}
---
+++ app/src/test/java/rs/ltt/android/ui/PreviewMeasurementsTest.java
@@ -0,0 +1,122 @@
+package rs.ltt.android.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PreviewMeasurementsTest {
+
+    @Test
+    public void landscape2048x1536PreviewOfSameRatio() {
+        final PreviewMeasurements previewMeasurements =
+                PreviewMeasurements.of(2048, 1536, 512, 384);
+        Assert.assertEquals(4, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(0, previewMeasurements.y);
+        Assert.assertEquals(512, previewMeasurements.width);
+        Assert.assertEquals(384, previewMeasurements.height);
+    }
+
+    @Test
+    public void landscape2048x1536PreviewOf169() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(2048, 1536, 160, 90);
+        Assert.assertEquals(8, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(27, previewMeasurements.y);
+        Assert.assertEquals(256, previewMeasurements.width);
+        Assert.assertEquals(144, previewMeasurements.height);
+    }
+
+    @Test
+    public void landscape2048x1536PreviewOf169Large() {
+        final PreviewMeasurements previewMeasurements =
+                PreviewMeasurements.of(2048, 1536, 1600, 900);
+        Assert.assertEquals(1, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(126, previewMeasurements.y);
+        Assert.assertEquals(2048, previewMeasurements.width);
+        Assert.assertEquals(1152, previewMeasurements.height);
+    }
+
+    @Test
+    public void landscape2048x1PreviewOf169Large() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(2048, 1, 1600, 900);
+        Assert.assertEquals(1, previewMeasurements.sampleSize);
+
+        Assert.assertEquals(224, previewMeasurements.x);
+
+        Assert.assertEquals(1600, previewMeasurements.width);
+        Assert.assertEquals(1, previewMeasurements.height);
+    }
+
+    @Test
+    public void square2048x2048PreviewOf169() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(2048, 2048, 160, 90);
+
+        Assert.assertEquals(8, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(27, previewMeasurements.y);
+        Assert.assertEquals(256, previewMeasurements.width);
+        Assert.assertEquals(144, previewMeasurements.height);
+    }
+
+    @Test
+    public void portrait1536x2048PreviewOf169() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(1536, 2048, 160, 90);
+        Assert.assertEquals(8, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(0, previewMeasurements.y);
+        Assert.assertEquals(192, previewMeasurements.width);
+        Assert.assertEquals(108, previewMeasurements.height);
+    }
+
+    @Test
+    public void portrait480x90PreviewOf169() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(480, 90, 160, 90);
+        System.out.println(previewMeasurements);
+        Assert.assertEquals(1, previewMeasurements.sampleSize);
+        Assert.assertEquals(160, previewMeasurements.x);
+        Assert.assertEquals(0, previewMeasurements.y);
+        Assert.assertEquals(160, previewMeasurements.width);
+        Assert.assertEquals(90, previewMeasurements.height);
+    }
+
+    @Test
+    public void portrait480x80PreviewOf169() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(480, 80, 160, 90);
+        Assert.assertEquals(1, previewMeasurements.sampleSize);
+        Assert.assertEquals(160, previewMeasurements.x);
+        Assert.assertEquals(0, previewMeasurements.y);
+        Assert.assertEquals(160, previewMeasurements.width);
+        Assert.assertEquals(80, previewMeasurements.height);
+    }
+
+    @Test
+    public void portrait1536x2048PreviewOf169Large() {
+        final PreviewMeasurements previewMeasurements =
+                PreviewMeasurements.of(1536, 2048, 1600, 900);
+        Assert.assertEquals(1, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(0, previewMeasurements.y);
+    }
+
+    @Test
+    public void portrait1x2048PreviewOf169Large() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(1, 2048, 1600, 900);
+        Assert.assertEquals(1, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(0, previewMeasurements.y);
+        Assert.assertEquals(1, previewMeasurements.width);
+        Assert.assertEquals(900, previewMeasurements.height);
+    }
+
+    @Test
+    public void portrait1x2PreviewOf169Large() {
+        final PreviewMeasurements previewMeasurements = PreviewMeasurements.of(1, 2, 1600, 900);
+        Assert.assertEquals(1, previewMeasurements.sampleSize);
+        Assert.assertEquals(0, previewMeasurements.x);
+        Assert.assertEquals(0, previewMeasurements.y);
+        Assert.assertEquals(1, previewMeasurements.width);
+        Assert.assertEquals(2, previewMeasurements.height);
+        System.out.println(previewMeasurements);
+    }
+}
