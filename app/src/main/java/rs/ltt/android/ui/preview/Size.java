154b89b7dda70ce8e86899ab9323514d9727ce74
Daniel Gultsch <daniel@gultsch.de>
2022-01-22 17:41:59 +0100

    fix pdf preview generation for rare aspect ratios

--- app/src/main/java/rs/ltt/android/ui/preview/Size.java
+++ app/src/main/java/rs/ltt/android/ui/preview/Size.java
@@ -1,8 +1,10 @@
 package rs.ltt.android.ui.preview;
 
 import androidx.annotation.NonNull;
+
 import com.google.common.base.MoreObjects;
 import com.google.common.math.IntMath;
+
 import java.math.RoundingMode;
 
 public class Size {
@@ -24,9 +26,24 @@ public class Size {
     }
 
     public Size scaleTo(final Size size) {
-        final int horizontalScale = IntMath.divide(size.width, width, RoundingMode.UP);
-        final int verticalScale = IntMath.divide(size.height, height, RoundingMode.UP);
-        final int scale = Math.max(horizontalScale, verticalScale);
-        return new Size(scale * width, scale * height);
+        if (width > size.width && height > size.height) {
+            final int horizontalScale = IntMath.divide(width, size.width, RoundingMode.DOWN);
+            final int verticalScale = IntMath.divide(height, size.height, RoundingMode.DOWN);
+            final int scale = Math.min(horizontalScale, verticalScale);
+            return new Size(width / scale, height / scale);
+        } else {
+            final int horizontalScale = IntMath.divide(size.width, width, RoundingMode.UP);
+            final int verticalScale = IntMath.divide(size.height, height, RoundingMode.UP);
+            final int scale = scale(horizontalScale, verticalScale);
+            return new Size(scale * width, scale * height);
+        }
+    }
+
+    private static int scale(final int horizontalScale, final int verticalScale) {
+        if (Math.abs(horizontalScale - verticalScale) > 3) {
+            return Math.min(horizontalScale, verticalScale);
+        } else {
+            return Math.max(horizontalScale, verticalScale);
+        }
     }
 }
---
+++ app/src/test/java/rs/ltt/android/ui/PreviewSizeTest.java
@@ -0,0 +1,54 @@
+package rs.ltt.android.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import rs.ltt.android.ui.preview.Size;
+
+public class PreviewSizeTest {
+
+    @Test
+    public void upscalePortraitPdf() {
+        final Size previewSize = new Size(726, 264);
+        final Size pageSize = new Size(210, 297);
+        final Size renderingSize = pageSize.scaleTo(previewSize);
+        Assert.assertEquals(840, renderingSize.width);
+        Assert.assertEquals(1188, renderingSize.height);
+    }
+
+    @Test
+    public void upscaleLandscapePdf() {
+        final Size previewSize = new Size(726, 264);
+        final Size pageSize = new Size(297, 210);
+        final Size renderingSize = pageSize.scaleTo(previewSize);
+        Assert.assertEquals(891, renderingSize.width);
+        Assert.assertEquals(630, renderingSize.height);
+    }
+
+    @Test
+    public void upscaleVeryLandscapePdf() {
+        final Size previewSize = new Size(726, 264);
+        final Size pageSize = new Size(200, 10);
+        final Size renderingSize = pageSize.scaleTo(previewSize);
+        Assert.assertEquals(800, renderingSize.width);
+        Assert.assertEquals(40, renderingSize.height);
+    }
+
+    @Test
+    public void downscalePortraitPdf() {
+        final Size previewSize = new Size(726, 264);
+        final Size pageSize = new Size(2100, 2970);
+        final Size renderingSize = pageSize.scaleTo(previewSize);
+        Assert.assertEquals(1050, renderingSize.width);
+        Assert.assertEquals(1485, renderingSize.height);
+    }
+
+    @Test
+    public void downscaleLandscapePdf() {
+        final Size previewSize = new Size(726, 264);
+        final Size pageSize = new Size(2970, 2100);
+        final Size renderingSize = pageSize.scaleTo(previewSize);
+        Assert.assertEquals(742, renderingSize.width);
+        Assert.assertEquals(525, renderingSize.height);
+    }
+}
