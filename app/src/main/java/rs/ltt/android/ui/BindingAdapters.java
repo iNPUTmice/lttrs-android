ebfe1107b28a452f04eb142ca88780ccadbd5334
Daniel Gultsch <daniel@gultsch.de>
2022-01-30 15:30:08 +0100

    fix code formatting

--- app/src/main/java/rs/ltt/android/ui/BindingAdapters.java
+++ app/src/main/java/rs/ltt/android/ui/BindingAdapters.java
@@ -376,7 +376,7 @@ public class BindingAdapters {
             final TextInputEditText editText, final @NonNull Supplier<Boolean> callback) {
         editText.setOnEditorActionListener(
                 (v, actionId, event) -> {
-                    //event is null when using software keyboard
+                    // event is null when using software keyboard
                     if (event == null || event.getAction() == KeyEvent.ACTION_UP) {
                         return Boolean.TRUE.equals(callback.get());
                     }
